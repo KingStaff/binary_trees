480da6ad923f9b02c4d21ac884bfabb051cb2509
kingStaff <karimawudulai@gmail.com>
2023-08-30 09:30:30 -0700

    fixes betty binary tree

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -53,16 +53,13 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t deep = 1;
+	size_t deep = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	if (tree->parent != NULL)
+	while (tree != NULL)
 	{
-		deep += binary_tree_depth(tree->parent);
-		return (deep);
+		deep ++;
+	        tree = tree->parent;
 	}
-	return (0);
+	return depth;
 
 }
